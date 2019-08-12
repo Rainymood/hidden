7a90b6186f2f93f4432511ecd2ce63c2f77efbee
Jan Meppe <janmeppe@hotmail.com>
2019-08-12 14:24:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Hidden
+
+Are you in an open office? Do you want some privacy when you are writing a note
+to yourself? 
+
+Try **hidden** now! 
+
+## Installation
+
+To install 
+
+    git clone
+    cd hidden/
+    python hidden.py
+
+## Usage
+
+To use
+
+    $ hidden
+    Note: *******
+    Adding note to ./notes.org...
+
+To view your note
+
+    $ cat notes.org
+    [2019-08-12 Mon 14:12]
+    Hello world!
+
+## TODO
+
+- Installation with `pip install hidden`. 
+- Write output to other filepaths, `with open(filepath, "a+")` seems to break for some
+  reason. 
---
+++ hidden.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+import click
+import os
+
+try:
+    import getch
+
+    def getpass(prompt):
+        """Replacement for getpass.getpass() which prints asterisks for each character typed. Stolen from: https://bit.ly/2YYE22X"""
+        print(prompt, end="", flush=True)
+        buf = ""
+        while True:
+            ch = getch.getch()
+            if ch == "\n":
+                print("")
+                break
+            else:
+                buf += ch
+                print("*", end="", flush=True)
+        return buf
+
+
+except ImportError:
+    from getpass import getpass
+
+
+def add_note(note, filepath):
+    """Writes note to file at filepath. 
+
+    Notes: 
+        File has to exist. If the file does not exist everything breaks.
+
+    Example format: 
+        [2019-08-12 Mon 11:16] 
+        Lorem ipsum dolor sit amet. 
+    """
+    date_org_fmt = datetime.strftime(datetime.now(), "[%Y-%m-%d %a %H:%M]")
+    content = date_org_fmt + "\n" + note + "\n\n"
+
+    with open(filepath, "a") as f:
+        # click.echo(f"Adding note to {filepath.split('.')[0]} list...")
+        click.echo(f"Adding note to ./notes.org...")
+        f.write(content)
+
+
+@click.command()
+@click.option("--filepath", default="./notes.org", help="Path to file.")
+def main(filepath):
+    note = getpass(prompt="Note: ")
+    add_note(note, filepath)
+
+
+if __name__ == "__main__":
+    main()
